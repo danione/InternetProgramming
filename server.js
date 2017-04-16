5782d1d3a661de8886d9a69c95caf7ba22471c61
danione <danione@abv.bg>
2017-04-16 23:42:23 +0300

    We have a db ( Mongo, yeah, bby)

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
+
 app.use('/static', express.static('static_pages'));
 
 app.get('/', function(req, res)
@@ -9,10 +10,55 @@ app.get('/', function(req, res)
   res.sendFile(__dirname + '/html_pages/index.html');
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(8089, function () {
 
   var host = server.address().address
   var port = server.address().port
 
   console.log("Example app listening at http://%s:%s", host, port)
+});
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/InternetProgramming';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  findDocuments(db, function(){
+    insertDocuments(db, function() {
+      db.close();
+    });
+  });
+
 });
