4f43bbc64e454fa46b5bfcf415ea5036984a04bb
Martin Galabov <martucha1@me.com>
2017-04-23 19:21:44 +0300

    Fixed login form to be in index html

--- static_pages/js_index.js
+++ static_pages/js_index.js
@@ -1,27 +1,24 @@
 // Get the modal
-var modal = document.getElementById('sign_up_form');
+var modal_sign_up = document.getElementById('sign_up_form');
+
+var modal_sing_in = document.getElementById('sign_in_form')
 
 var link = document.getElementById('sign_in_link');
 
-var login_cancel = document.getElementById('login_cancel');
 
-var login_window;
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal_sign_up) {
+        modal_sign_up.style.display = "none";
+    } else if (event.target == modal_sing_in) {
+        modal_sing_in.style.display = "none";
     }
 }
 
 link.onclick = function(event) {
-  modal.style.display = "none";
-  var x = screen.width/2 - 300/2;
-  var y = screen.height/2 - 400/2;
-  var login_window = window.open("", "", "left="+x+",top="+y+",width=300,height=400,menubar=no");
-  $.get("static/login.html", function(html_string)
-  {
-     login_window.document.write(html_string);
-  },'html');
+  modal_sign_up.style.display = "none";
+  document.getElementById('sign_in_form').style.display='block'
 }
 
 
