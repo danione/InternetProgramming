93e3f139b083ecf10d82b53635b5712cd210fec1
Martin Galabov <martucha1@me.com>
2017-04-23 18:07:41 +0300

    Added login page

--- static_pages/js_index.js
+++ static_pages/js_index.js
@@ -1,12 +1,29 @@
 // Get the modal
-var modal = document.getElementById('sign_in_form');
+var modal = document.getElementById('sign_up_form');
 
+var link = document.getElementById('sign_in_link');
+
+var login_cancel = document.getElementById('login_cancel');
+
+var login_window;
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+link.onclick = function(event) {
+  modal.style.display = "none";
+  var x = screen.width/2 - 300/2;
+  var y = screen.height/2 - 400/2;
+  var login_window = window.open("", "", "left="+x+",top="+y+",width=300,height=400,menubar=no");
+  $.get("static/login.html", function(html_string)
+  {
+     login_window.document.write(html_string);
+  },'html');
+}
+
 
 $.validate(
   {
